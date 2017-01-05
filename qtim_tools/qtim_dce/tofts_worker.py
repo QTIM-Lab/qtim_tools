b96c12f98702c02d384b19e6f30c4d8b3b879ee3
AndrewBeers <andrew_beers@alumni.brown.edu>
2017-01-05 17:55:03 -0500

    Added test function for package
    ibid..

--- qtim_tools/qtim_dce/tofts_worker.py
+++ qtim_tools/qtim_dce/tofts_worker.py
@@ -1,5 +1,8 @@
 import tofts_parameter_calculator
 # from ..qtim_utilities import nifti_util
 
+def test(filepath=[]):
+	tofts_parameter_calculator.test_method_3d(filepath)
+
 if __name__ == '__main__':
 	tofts_parameter_calculator.test_method_3d()
