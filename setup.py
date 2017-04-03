0d4ac6d70ed50d5269cc03cfc4751b7e8365c125
AndrewBeers <andrew_beers@alumni.brown.edu>
2017-04-03 17:04:00 -0400

    Version .12!

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ from os import path
 setup(
   name = 'qtim_tools',
   # packages = ['qtim_tools'], # this must be the same as the name above
-  version = '0.1.11',
+  version = '0.1.12',
   description = 'A library for medical imaging analysis, with an emphasis on MRI, machine learning, and neuroimaging. Created by the Quantiative Tumor Imaging Lab at the Martinos Center (Harvard-MIT Program in Health, Sciences, and Technology / Massachussets General Hospital)',
   packages=find_packages(),
   author = 'Andrew Beers',
   author_email = 'abeers@mgh.harvard.edu',
   url = 'https://github.com/QTIM-Lab/qtim_tools', # use the URL to the github repo
-  download_url = 'https://github.com/QTIM-Lab/qtim_tools/tarball/0.1.11',
+  download_url = 'https://github.com/QTIM-Lab/qtim_tools/tarball/0.1.12',
   keywords = ['neuroimaging', 'niftis', 'nifti','mri','dce','dsc','ktrans','ve','tofts','machine learning','vision','texture','learning'], # arbitrary keywords
   install_requires=['nibabel','pydicom','pynrrd','matplotlib','numpy','scipy'],
   classifiers = [],
