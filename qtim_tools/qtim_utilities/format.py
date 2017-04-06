058b6997d2df80224bd8953bd83e62364e38b8d6
AndrewBeers <andrew_beers@alumni.brown.edu>
2017-04-06 19:39:36 -0400

    TODO: Combine format with file_util. See notes for further changes needed.

--- qtim_tools/qtim_utilities/format.py
+++ qtim_tools/qtim_utilities/format.py
@@ -2,7 +2,8 @@
     medical imaging (and perhaps other) formats.
 """
 
-
+from nifti_util import nifti_2_numpy
+from dicom_util import dcm_2_numpy
 
 # This is magic code for Python 3 compatability. Of course
 # this package isn't Python 3 compatible, but why not start now.
@@ -11,12 +12,27 @@ try:
 except NameError:
   basestring = str
 
-FORMAT_LIST = {'dicom':['*.dcm','*.ima','*'],
-                'nifti':['*.nii','*.nii.gz'],
-                'nrrd':['*.nrrd','*.nhdr']}
+FORMAT_LIST = {'dicom':['.dcm','.ima'],
+                'nifti':['.nii','.nii.gz'],
+                'nrrd':['.nrrd','.nhdr'],
+                'image':['.jpg','.png']}
 
-def convert_input_2_numpy(input)
+def check_format(filepath):
+
+    return
+
+
+
+def convert_input_2_numpy(input, input_format=[]):
+    
+    """ This function is meant to take in any normal imaging file 
+        format and convert it into numpy. Numpy is supposed from
+        now on to be the lingua franca of qtim_tools.
+    """
+
     if isinstance(input, basestring):
+        if input_format == []:
+            format = check_format(input)
         pass
     else:
         return input
