77ca2cd957034d80704df524f838645ff04692a7
AndrewBeers <Andrew_Beers@alumni.brown.edu>
2017-05-10 16:48:43 -0400

    fix: qtim_features init hotfix
    Also creation of qtim_viz.

--- qtim_tools/__init__.py
+++ qtim_tools/__init__.py
@@ -3,6 +3,6 @@ import qtim_dce
 import qtim_learning
 import qtim_utilities
 import qtim_preprocessing
-import test_data
-# import qtim_visualization
+import qtim_visualization
 # import qtim_pipelines
+import test_data
--- qtim_tools/qtim_features/__init__.py
+++ qtim_tools/qtim_features/__init__.py
@@ -4,10 +4,3 @@ import GLCM
 import morphology
 import statistics
 import phantoms
-import document_test
-
-def main():
-    """This is the entry point of the application!
-    :raises: :class:`KeyboardInterrupt`, :class:`SystemExit`
-    """
-    pass
