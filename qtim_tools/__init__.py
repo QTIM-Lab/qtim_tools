4ef8cd476100c608b731c176d004004ffac78d1b
AndrewBeers <Andrew_Beers@alumni.brown.edu>
2017-05-15 14:27:27 -0400

    fix: Unload broken qtim_viz

--- qtim_tools/__init__.py
+++ qtim_tools/__init__.py
@@ -3,6 +3,6 @@ import qtim_dce
 import qtim_learning
 import qtim_utilities
 import qtim_preprocessing
-import qtim_visualization
+# import qtim_visualization
 # import qtim_pipelines
 import test_data
