c2c6c72a38d434dbc3bc558d2ec50e89b65349a2
AndrewBeers <Andrew_Beers@alumni.brown.edu>
2017-05-15 14:21:32 -0400

    feature: Sphinx documentation, numpy, ReadTheDocs

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -54,7 +54,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'qtim_tools'
 
 # General information about the project.
 project = u'qtim_tools'
@@ -119,7 +119,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
