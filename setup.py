e29d8ba9073991545c74b6e52dbbca7395232655
AndrewBeers <andrew_beers@alumni.brown.edu>
2018-03-08 17:17:16 -0500

    docs: import error pydicom

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setup(
   url = 'https://github.com/QTIM-Lab/qtim_tools', # use the URL to the github repo
   download_url = 'https://github.com/QTIM-Lab/qtim_tools/tarball/0.1.13',
   keywords = ['neuroimaging', 'niftis', 'nifti','mri','dce','dsc','ktrans','ve','tofts','machine learning','vision','texture','learning'], # arbitrary keywords
-  install_requires=['nibabel','pydicom','pynrrd','Pillow','configparser','matplotlib','numpy','scipy','scikit-image', 'pyyaml', 'beautifulsoup4'],
+  install_requires=['nibabel','dicom','pynrrd','Pillow','configparser','matplotlib','numpy','scipy','scikit-image', 'pyyaml', 'beautifulsoup4'],
   classifiers = [],
 )
